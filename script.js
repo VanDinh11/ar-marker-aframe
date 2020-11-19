e250eb0ec194545c9af744bcc36cd537fb1abd64
VanDinh11 <dinhpv.11.96@gmail.com>
2020-11-19 09:48:29 +0700

    add event click

---
+++ script.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('button', {
+    init: function () {
+
+        const gltf = document.querySelector('#dinoId');
+        var x = gltf.getAttribute('scale').x;
+        var y = gltf.getAttribute('scale').y;
+        var z = gltf.getAttribute('scale').z;
+
+        // every click, we make our model grow in size :)
+        gltf.addEventListener('click', function (ev, target) {
+            console.log(gltf.getAttribute('scale'));
+            gltf.setAttribute('scale', x + " " + y + " " + z);
+            x += 0.1;
+            y += 0.1;
+            z += 0.1;
+        });
+    }
+});
